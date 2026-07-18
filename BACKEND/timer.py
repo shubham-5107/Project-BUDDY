eac5a11b81000cbde556d351cd96ccec9beb488e
SHUBHAM SHARMA <shubham.pbt007@gmail.com>
2026-07-18 21:45:52 +0530

    Update timer.py

--- BACKEND/timer.py
+++ BACKEND/timer.py
@@ -1,31 +1,83 @@
 import time
-import pyttsx3
 
 class timer:
-    @staticmethod
-    def speak(audio):
-        engine= pyttsx3.init()
-        engine.setProperty('rate', 170)
-        engine.say(audio)
-        engine.runAndWait()
+    def paraphraser(self, query):
+                l1=[]
+                for i in query.split():
+                    if i.isdigit():
+                        l1.append(i)
+                l2=len(l1)
 
-    def __init__(self,hours,minutes=0,seconds=0):
-        self.hours= float(hours)
-        self.minutes= float(minutes)
-        self.seconds= float(seconds)
+                l=[]
+                if l2==3:
+                    for i in query.split():
+                        try:
+                            l.append(str(int(i)))
+                        except Exception as e:
+                            pass
+                
+                elif l2==2:
+                    for i in range(len(query.split())):
+                        if query.split()[i]=='hours':
+                            l.append(query.split()[i-1])
+                            for j in range(len(query.split())):
+                                if query.split()[j]=='minutes':
+                                    l.append(query.split()[j-1])
+                                    l.append('0')
+                                    break
+
+                                elif query.split()[j]=='seconds':
+                                    l.append(0)
+                                    l.append(query.split()[j-1])
+                                    break
+                            break 
+                        
+                        elif query.split()[i]=='minutes':
+                            l.append(0)
+                            l.append(query.split()[i-1])
+                            for j in range(len(query.split())):
+                                if query.split()[j] == 'seconds':
+                                    l.append(query.split()[j-1])
+                                    break
+                            break
+                        
+                        elif query.split()[i]=='seconds':
+                            l.append(0)
+                            l.append(0)
+                            l.append(query.split()[i-1])
+                            break 
+                elif l2==1:
+                    for i in range(len(query.split())):
+                        if query.split()[i] == 'hours':
+                            l.append(query.split()[i-1])
+                            l.append(0)
+                            l.append(0)
+                            break
+                        elif query.split()[i] == 'minutes':
+                            l.append(0)
+                            l.append(query.split()[i-1])
+                            break
+                        elif query.split()[i] == 'seconds':
+                            l.append(0)
+                            l.append(0)
+                            l.append(query.split()[i-1])
+                            break
+                return l
+    
+    def buzzer(self, l):
+        self.hours= float(l[0])
+        self.minutes= float(l[1])
+        self.seconds= float(l[2])
 
         sleep_value= self.hours*3600+ self.minutes*60+self.seconds
 
         time.sleep(sleep_value)
-        self.speak('Timer Ended')
+        print('Timer Ended')
         time.sleep(.1)
-        self.speak('Timer Ended')
+        print('Timer Ended')
         time.sleep(.1)
- 
-
-            
+        print('Timer Ended')
 
 
-if __name__=='__main__':
-    obj= timer(
-
+if __name__ == "__main__":
+    obj= timer()
